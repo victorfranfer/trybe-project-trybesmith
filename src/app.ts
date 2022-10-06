ab44024b50c5da49d92cb44ce299d58fb1f5a446
VictorFranciscoFerreira <victorfranciscoferreira@gmail.com>
2022-10-06 20:01:03 -0300

    req1:feito

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express from 'express';
-//PR
+import productRouter from './routes/product.routes';
+
 const app = express();
 
 app.use(express.json());
+
+app.use('/products', productRouter);
 
 export default app;
